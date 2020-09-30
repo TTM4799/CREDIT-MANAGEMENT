0427ed33954c6278a0814b5646031807b6d567e0
TTM4799 <66559518+TTM4799@users.noreply.github.com>
2020-09-30 17:10:56 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,21 @@
+<body>
+        
+        <nav class="navb">
+			<input type="checkbox" id="check">
+			<label for="check">
+					<i class="fa fa-bars" id="btn"></i>
+					<i class="fa fa-times" id="cancel"></i>
+			</label>
+					
+
+							<ul>
+									<li><a href="index.php">HOME</a></li>
+									<li><a href="users.php">USERS</a>
+									</li>
+									<li> <a href="history.php"> TRANSFER HISTORY</a>
+									</li>
+									<li><a href="transfer.php">CREDIT TRANSFER</a>
+									</li>
+							</ul>
+
+        </nav>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Credit Management</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<?php
+include 'header.php';
+?>
+
+
+
+<div class="index">
+    <h1 style="color:yellow">TRANSFER</h1>
+    <p><h2 style="color:yellow">Website in which credit transfer can be done easily.....<h2></p>
+</div>
+</body>
+
+</html>
